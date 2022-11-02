e1f54441a1669f5643ffd52337165b08d154a961
User <asd>
2022-11-02 15:25:15 +0100

    dodane dzialanie buttona do pierwszego i ostatniego elementu. lekka modyfikacja buttonow od dodawanie i usuwania

--- app/src/main/java/com/example/kolejka_i_stos/MainActivity.kt
+++ app/src/main/java/com/example/kolejka_i_stos/MainActivity.kt
@@ -20,18 +20,18 @@ class MainActivity : AppCompatActivity() {
         val txt_ile_w_kolejce = findViewById<TextView>(R.id.ile_w_kolejce)
         val txt_pierwszy_w_kolejce = findViewById<TextView>(R.id.pierwszy_w_kolejce)
         val txt_ostatni_w_kolejce = findViewById<TextView>(R.id.ostatni_w_kolejce)
-
+        var pierw = 0
+        var ost = 0
         val length = Toast.LENGTH_SHORT
-        var text = ""
         var numer = 1
         var ilosc_osob_w_kolejce = 0
 
         butt_dodaj_do_kolejki.setOnClickListener {
+            Toast.makeText(applicationContext,"Twój numer to: " + numer, length).show()
             kolejka.add(numer)
             numer+=1
             ilosc_osob_w_kolejce+=1
             txt_ile_w_kolejce.text = ilosc_osob_w_kolejce.toString()
-
         }
 
         butt_usun_z_kolejki.setOnClickListener {
@@ -45,8 +45,20 @@ class MainActivity : AppCompatActivity() {
             }
             else
             {
-                text = "Brak osob w kolejce"
-                Toast.makeText(applicationContext, text, length).show()
+                Toast.makeText(applicationContext, "Brak osob w kolejce", length).show()
+            }
+        }
+        butt_pierw_i_ost_w_kolejce.setOnClickListener {
+            if(!kolejka.isEmpty())
+            {
+                pierw = kolejka.element()
+                ost = kolejka.last()
+                txt_pierwszy_w_kolejce.text = pierw.toString()
+                txt_ostatni_w_kolejce.text = ost.toString()
+            }
+            else
+            {
+                Toast.makeText(applicationContext, "Brak osob w kolejce", length).show()
             }
         }
     }
