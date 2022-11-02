0b0c62753a785033c5898913c95f0143bade885e
User <asd>
2022-11-02 14:31:42 +0100

    initial commit

---
+++ app/src/main/java/com/example/kolejka_i_stos/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.kolejka_i_stos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
